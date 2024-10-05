bd733f6ff7923863f1b5462781675141a0fb26ca
John-James-Knapp <john.james.knapp2@gmail.com>
2024-10-05 15:36:09 -0400

    created chars_dict_to_sorted_list to create a sort_list of the char dict and sort_on function to help with the sort

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+# this function is called by chars_dict_to_sorted_list when it sorts the list,
+# this helps tell the sort to go by the "num" associated with each char from highest to lowest.
+def sort_on(d):
+    return d["num"]
+
+# Here we are creating a list for each char in the dict of nums and chars.
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 # create a dict with chars that appear in the text and how many times they appear
 def get_dict_chars(text):
     chars = {}
